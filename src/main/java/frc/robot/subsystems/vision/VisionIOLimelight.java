dda8ce5e84fdc9d130094c856ee05711f19e7d07
Caden Dalley <caden.dalley@icloud.com>
2025-11-06 00:20:12 -0700

    Add throttle control to VisionIOLimelight

--- src/main/java/frc/robot/subsystems/vision/VisionIOLimelight.java
+++ src/main/java/frc/robot/subsystems/vision/VisionIOLimelight.java
@@ -4,10 +4,8 @@ import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.networktables.DoubleArrayPublisher;
-import edu.wpi.first.networktables.DoubleArraySubscriber;
-import edu.wpi.first.networktables.DoubleSubscriber;
-import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.*;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotController;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -20,6 +18,8 @@ public class VisionIOLimelight implements VisionIO {
 
   private final Supplier<Rotation2d> rotationSupplier;
   private final DoubleArrayPublisher orientationPublisher;
+
+  private final DoublePublisher throttlePublisher;
 
   private final DoubleSubscriber latencySubscriber;
   private final DoubleSubscriber txSubscriber;
@@ -38,6 +38,7 @@ public class VisionIOLimelight implements VisionIO {
     this.name = name;
     this.rotationSupplier = rotationSupplier;
     orientationPublisher = table.getDoubleArrayTopic("robot_orientation_set").publish();
+    throttlePublisher = table.getDoubleTopic("throttle_set").publish();
     latencySubscriber = table.getDoubleTopic("tl").subscribe(0.0);
     txSubscriber = table.getDoubleTopic("tx").subscribe(0.0);
     tySubscriber = table.getDoubleTopic("ty").subscribe(0.0);
@@ -49,6 +50,9 @@ public class VisionIOLimelight implements VisionIO {
   @Override
   public void updateInputs(VisionIOInputs inputs) {
     inputs.name = name;
+
+    // Update throttle (prevents overheating when disabled)
+    throttlePublisher.set(DriverStation.isEnabled() ? 0.0 : 150.0);
 
     // Update connection status based on whether an update has been seen in the last 250ms
     inputs.connected =
